e7e5a9f6b65b1e781214eb10f840d50461607b58
DavideMara <davidemaracaglia@yahoo.com>
2024-10-30 22:51:32 +0100

    message

---
+++ davide_maracaglia.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("inserire il lato A: ");
+    scanf("")
+
+    scanf("%d %d %d",&a,&b,&c);
+
+    if (a , b , c > 0 && a+b > c, b+c> a, c+a>b && a > b-c, a > c-b, b > a-c, b > c-a, c > a-b, c > b-a){
+        printf("e' andata capo \n");
+    }
+
+
+    
+return 0;
+}
